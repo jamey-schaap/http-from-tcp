0b3e806baf763d1d8276de565415b2d4d18b6ff7
Jamey Schaap <jameyschaap@proton.me>
2025-10-12 21:56:13 +0200

    Add initial http server

---
+++ internal/server/server.go
@@ -0,0 +1,64 @@
+package server
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"sync/atomic"
+)
+
+type Server struct {
+	listener net.Listener
+	closed   atomic.Bool
+}
+
+func Serve(port int) (*Server, error) {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return nil, err
+	}
+
+	s := &Server{
+		listener: listener,
+	}
+
+	go s.listen()
+	return s, nil
+}
+
+func (s *Server) Close() error {
+	s.closed.Store(true)
+	if s.listener != nil {
+		return s.listener.Close()
+	}
+	return nil
+}
+
+func (s *Server) listen() {
+	for {
+		conn, err := s.listener.Accept()
+		if err != nil {
+			if s.closed.Load() {
+				return
+			}
+
+			log.Printf("Error accepting connection: %v", err)
+			continue
+		}
+		go s.handle(conn)
+	}
+}
+
+func (s *Server) handle(conn net.Conn) {
+	defer conn.Close()
+	res := "HTTP/1.1 200 OK\r\n" +
+		"Content-Type: text/plain\r\n" +
+		"Content-Length: 13\r\n" +
+		"\r\n" +
+		"Hello World!\n"
+
+	_, err := conn.Write([]byte(res))
+	if err != nil {
+		log.Printf("Error writing response: %v", err)
+	}
+}
