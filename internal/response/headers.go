b22f02421203a8643cca039aee355ba7813e2966
Jamey Schaap <jameyschaap@proton.me>
2025-10-14 21:32:29 +0200

    Add http handler

---
+++ internal/response/headers.go
@@ -0,0 +1,26 @@
+package response
+
+import (
+	"fmt"
+	"http-from-tcp/internal/headers"
+	"io"
+)
+
+func GetDefaultHeaders(contentLen int) headers.Headers {
+	h := headers.NewHeaders()
+	h.Set("Content-Length", fmt.Sprintf("%d", contentLen))
+	h.Set("Connection", "close")
+	h.Set("Content-Type", "text/plain")
+	return h
+}
+
+func WriteHeaders(w io.Writer, headers headers.Headers) error {
+	for k, v := range headers {
+		_, err := w.Write([]byte(fmt.Sprintf("%s: %s\r\n", k, v)))
+		if err != nil {
+			return err
+		}
+	}
+	_, err := w.Write([]byte("\r\n"))
+	return err
+}
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,27 +1,46 @@
 package server
 
 import (
+	"bytes"
 	"fmt"
+	"http-from-tcp/internal/request"
+	"http-from-tcp/internal/response"
+	"io"
 	"log"
 	"net"
 	"sync/atomic"
 )
 
+type Handler func(w io.Writer, req *request.Request) *HandlerError
+
+type HandlerError struct {
+	StatusCode response.StatusCode
+	Message    string
+}
+
+func (he HandlerError) Write(w io.Writer) {
+	response.WriteStatusLine(w, he.StatusCode)
+	messageBytes := []byte(he.Message)
+	headers := response.GetDefaultHeaders(len(messageBytes))
+	response.WriteHeaders(w, headers)
+	w.Write(messageBytes)
+}
+
 type Server struct {
+	handler  Handler
 	listener net.Listener
 	closed   atomic.Bool
 }
 
-func Serve(port int) (*Server, error) {
+func Serve(port int, handler Handler) (*Server, error) {
 	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
 	if err != nil {
 		return nil, err
 	}
-
 	s := &Server{
 		listener: listener,
+		handler:  handler,
 	}
-
 	go s.listen()
 	return s, nil
 }
@@ -41,7 +60,6 @@ func (s *Server) listen() {
 			if s.closed.Load() {
 				return
 			}
-
 			log.Printf("Error accepting connection: %v", err)
 			continue
 		}
@@ -51,14 +69,27 @@ func (s *Server) listen() {
 
 func (s *Server) handle(conn net.Conn) {
 	defer conn.Close()
-	res := "HTTP/1.1 200 OK\r\n" +
-		"Content-Type: text/plain\r\n" +
-		"Content-Length: 13\r\n" +
-		"\r\n" +
-		"Hello World!\n"
 
-	_, err := conn.Write([]byte(res))
+	req, err := request.RequestFromReader(conn)
 	if err != nil {
-		log.Printf("Error writing response: %v", err)
+		hErr := &HandlerError{
+			StatusCode: response.StatusCodeBadRequest,
+			Message:    err.Error(),
+		}
+		hErr.Write(conn)
+		return
 	}
+
+	buf := bytes.NewBuffer([]byte{})
+	hErr := s.handler(buf, req)
+	if hErr != nil {
+		hErr.Write(conn)
+		return
+	}
+	b := buf.Bytes()
+	response.WriteStatusLine(conn, response.StatusCodeSuccess)
+	headers := response.GetDefaultHeaders(len(b))
+	response.WriteHeaders(conn, headers)
+	conn.Write(b)
+	return
 }
